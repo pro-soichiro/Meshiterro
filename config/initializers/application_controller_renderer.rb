4906ae5f1700dfea89e141338fb7792100fffcb6
pro-soichiro <dr.1987626@gmail.com>
2021-09-09 09:28:30 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b60de15e14c0e5064388e097d0b51a41b1f1973e28db6bc5cdf95735a4c8167bc88e8ab2468703d566c95d2ede928a3aafb86c29bb05577afa7a741441049b32'
